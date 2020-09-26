d6055ea611b163a57760f6f731e79be48f83d5ff
Davino Junior <>
2020-09-26 16:40:45 -0300

    Finished auth flow

--- index.js
+++ index.js
@@ -1,33 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport'); // not returning anything, so we can execute it directly
 
+mongoose.connect(keys.mongoURI);
 const app = express();
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback', // route for user coming back to application after checking client's ID and secret
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log(accessToken);
-      console.log(refreshToken);
-      console.log(profile);
-    }
-  )
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 1000, // 30 days
+        keys: [keys.cookieKey]
+    })
 );
 
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email'],
-  })
-);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/auth/google/callback', passport.authenticate('google'));
+require('./routes/authRoutes')(app); // returns the function and immediatly invokes passing the app
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
